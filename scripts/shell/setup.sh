38f47f5d46c4a16d9ef40187e5efe44d0108d40f
Jannis Schreiber <jannis.schreiber@mobilityhouse.com>
2024-08-30 13:02:58 +0200

    chore: upgrade setup scripts to sf cli

--- scripts/shell/setup.sh
+++ scripts/shell/setup.sh
@@ -21,21 +21,21 @@ echo "Installing node dependencies ..."
 npm ci
 
 if [ -z "$devhubusername" ]; then
-    echo "sfdx force:org:create -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -d "$duration" -f "$configFile" -a "$alias" -s
+    echo "sf org create scratch -d $duration -f $configFile -a $alias -s"
+    sf org create scratch -d "$duration" -f "$configFile" -a "$alias" -s
 else
-    echo "sfdx force:org:create -v $devhubusername -d $duration -f $configFile -a $alias -s"
-    sfdx force:org:create -v "$devhubusername" -d "$duration" -f "$configFile" -a "$alias" -s
+    echo "sf org create scratch -v $devhubusername -d $duration -f $configFile -a $alias -s"
+    sf org create scratch -v "$devhubusername" -d "$duration" -f "$configFile" -a "$alias" -s
 fi
 
 echo "Installing dependencies on $alias..."
 bash scripts/shell/dependency-install.sh -u "$alias"
 
-echo "sfdx force:source:push -u $alias"
-sfdx force:source:push -u "$alias"
+echo "sf project deploy start -u $alias"
+sf project deploy start -u "$alias"
 
-echo "sfdx force:data:tree:import -p data/plans/standard-plan.json -u $alias"
-sfdx force:data:tree:import -p data/plans/standard-plan.json -u "$alias"
+echo "sf data import tree -p data/plans/standard-plan.json -u $alias"
+sf data import tree -p data/plans/standard-plan.json -u "$alias"
 
-echo "sfdx force:org:open -u $alias -p \"/lightning/setup/SetupOneHome/home\""
-sfdx force:org:open -u "$alias" -p "/lightning/setup/SetupOneHome/home"
+echo "sf org open -u $alias -p \"/lightning/setup/SetupOneHome/home\""
+sf org open -u "$alias" -p "/lightning/setup/SetupOneHome/home"
